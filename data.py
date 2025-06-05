59752c952c9d052f50d90928d20dd7676d97ac54
AhmedSaleem1 <ahmedasaleem1@gmail.com>
2025-06-05 00:16:28 -0400

    Your commit message here

--- data.py
+++ data.py
@@ -1,7 +1,4 @@
-# Urban Routes test data
-
-URBAN_ROUTES_URL = ' https://cnt-6b3e273a-1b66-4637-9949-c1965248a2ab.containerhub.tripleten-services.com'
-
+URBAN_ROUTES_URL = 'https://cnt-6b3e273a-1b66-4637-9949-c1965248a2ab.containerhub.tripleten-services.com'
 ADDRESS_FROM = 'East 2nd Street, 601'
 ADDRESS_TO = '1300 1st St'
 PHONE_NUMBER = '+1 123 123 12 12'
