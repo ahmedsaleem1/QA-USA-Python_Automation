ec1ba317a92b5ae92cc03f80312e67b6cd98444c
AhmedSaleem1 <ahmedasaleem1@gmail.com>
2025-06-04 20:54:15 -0400

    Initial commit for Sprint 7

---
+++ data.py
@@ -0,0 +1,10 @@
+# Urban Routes test data
+
+URBAN_ROUTES_URL = ' https://cnt-6b3e273a-1b66-4637-9949-c1965248a2ab.containerhub.tripleten-services.com'
+
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
